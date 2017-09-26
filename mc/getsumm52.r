c11ea88e8badfb00857fbabf89b6991bb6e3dcdc
Quentin Read <quentin.read@gmail.com>
2017-09-26 14:07:27 -0400

    Update summary script for new trace plot

--- mc/getsumm52.r
+++ mc/getsumm52.r
@@ -44,3 +44,29 @@ for (i in 1:nrow(sm)) {
 	print(i)
 	trace_summ(species=sm[i,1], modelno=sm[i,2])
 }
+
+trace_summ_indivmodel <- function(bin, nchains) {
+	filenames <- paste0('bin', bin, 'samples', 1:nchains, '.csv')
+	library(rstan)
+	library(bayesplot)
+	fit_list <- list()
+	for (i in filenames) fit_list[[length(fit_list) + 1]] <- read_stan_csv(i)
+	mc_fit <- sflist2stanfit(fit_list)
+	
+	# Make trace plots
+	par_list <- c('lambda','beta_size','beta_temp','beta_prec','gamma', 'alpha[1]', 'alpha[2]', 'alpha[3]', 'sigma_g','mu_alpha', 'sigma_alpha', 'mu[1]','mu[2]','mu[3]','sigma_i[1]','sigma_i[2]','sigma_i[3]')
+	pdf(paste0('./figs/bin', bin,  '_traceplots.pdf'), height = 6, width = 10)
+		print(mcmc_trace(as.array(mc_fit, pars = par_list[1])))
+		print(mcmc_trace(as.array(mc_fit, pars = par_list[2])))
+		print(mcmc_trace(as.array(mc_fit, pars = par_list[3])))
+		print(mcmc_trace(as.array(mc_fit, pars = par_list[4])))
+		print(mcmc_trace(as.array(mc_fit, pars = par_list[5])))
+		print(mcmc_trace(as.array(mc_fit, pars = par_list[6:8])))
+		print(mcmc_trace(as.array(mc_fit, pars = par_list[10:11])))
+		print(mcmc_trace(as.array(mc_fit, pars = par_list[12:14])))
+		print(mcmc_trace(as.array(mc_fit, pars = par_list[15:17])))
+	dev.off()
+	
+	summ <- summary(mc_fit, pars = c('beta_size', 'beta_temp', 'beta_prec', 'gamma', 'sigma_g', 'mu_alpha', 'sigma_alpha', 'lambda', 'alpha'))
+	save(summ, file = paste0('./summaries/bin', bin, '_summ.r'))
+}
